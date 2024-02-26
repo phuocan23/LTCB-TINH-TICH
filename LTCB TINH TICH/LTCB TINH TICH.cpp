b9546b138169531d6f46f7e562bf8efe488c35df
ADMIN <ADMIN@LAB1-MAY22>
2024-02-26 08:59:44 +0700

    Add project files.

---
+++ LTCB TINH TICH/LTCB TINH TICH.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, tich;
+	cout << "CHUONG TRINH TINH TICH\n";
+	cout << "Nhap so thu nhat:";
+	cin >> so1;
+	cout << "Nhap so thu hai:";
+	cin >> so2;
+	tich = so1 * so2;
+	cout << so1 << "*" << so2 << "=" << tich << endl;
+
+	return 0;
+}
+
+
